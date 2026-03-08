d8d4152f2c9ae1e483437abfab2209daa530f739
anastasiya kuts <nas5772@yandex.ru>
2026-03-09 01:45:13 +0300

    Start edit code with video of hw

--- src/test/java/tests/AutomationPracticeFormTests.java
+++ src/test/java/tests/AutomationPracticeFormTests.java
@@ -3,6 +3,7 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -11,25 +12,27 @@ public class AutomationPracticeFormTests extends TestBase {
     @Test
     void successfulFillFormTest() {
         open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+
         $("[id=firstName]").setValue("Ivan");
         $("[id=lastName]").setValue("Ivanov");
         $("[id=userEmail]").setValue("ivan@ivanov.com");
-        $("[id=gender-radio-1]").setValue("Male");
- //       $("[id=gender-radio-2]").setValue("true address");
-//        $("[id=gender-radio-3]").setValue("true address");
-        $("[id=userNumber]").setValue("1234567890");
-        $("[id=dateOfBirthInput]").setValue("05 Mar 2026");
-        $("[id=subjectsInput]").setValue("true info");
 
-//        $("[id=hobbies-checkbox-1]").setValue("1");
-        $("[id=hobbies-checkbox-2]").setValue("2");
-        $("[id=hobbies-checkbox-3]").setValue("3");
- //       $("[id=uploadePicture]").setValue("true address");
+//        $("[id=gender-radio-2]").parent().click();
+//        $(byText("Other")).click();
+        $("#genterWrapper").$(byText("Other")).click();
+        $("[id=dateOfBirthInput]").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+//        $(".react-datepicker__month-select").selectOptionByValue("6");
+
+        $("[id=userNumber]").setValue("1234567890");
+
+
 
         $("[id=currentAddress]").setValue("true address");
 
-        $("[id=react-select-3-live-region]").setValue("Uttar Pradesh");
-        $("[id=react-select-4-live-region]").setValue("Agra");
 
         $("[id=submit]").click();
 
