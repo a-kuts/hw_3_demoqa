7530bf6246dfa1af199120d5062391f6a061c1d6
anastasiya kuts <nas5772@yandex.ru>
2026-03-12 23:40:51 +0300

    Added homework

--- src/test/java/tests/AutomationPracticeFormTestsWithComments.java
+++ src/test/java/tests/AutomationPracticeFormTestsWithComments.java
@@ -1,16 +1,14 @@
 package tests;
 
-import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class AutomationPracticeFormTests extends TestBase {
+public class AutomationPracticeFormTestsWithComments extends TestBase {
 
     @Test
     void successfulFillFormTest() {
@@ -21,14 +19,20 @@ public class AutomationPracticeFormTests extends TestBase {
         $("[id=firstName]").setValue("Ivan");
         $("[id=lastName]").setValue("Ivanov");
         $("[id=userEmail]").setValue("ivan@ivanov.com");
+//        $("[id=gender-radio-2]").parent().click();
+//        $(byText("Other")).click();
         $("#genterWrapper").$(byText("Other")).click();
         $("[id=userNumber]").setValue("1234567890");
+
         $("[id=dateOfBirthInput]").click();
         $(".react-datepicker__month-select").selectOption("July");
+//        $(".react-datepicker__month-select").selectOptionByValue("6");
         $(".react-datepicker__year-select").selectOption("2008");
+//        $(".react-datepicker__month-select").selectOptionByValue("6");
         $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
         $("[id=subjectsInput]").setValue("Math").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
+//        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
         $("#uploadPicture").uploadFromClasspath("img/1.png");
         $("[id=currentAddress]").setValue("true address");
         $("[id=state]").click();
